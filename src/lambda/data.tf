c048279e550948194afa7dd200d1f3bd7b002ac3
hemanthreddyjonnala <hemanthreddyjonnala@gmail.com>
2024-01-26 18:47:02 -0500

    cleanup

--- src/lambda/data.tf
+++ src/lambda/data.tf
@@ -1,33 +1,3 @@
-# # Retrieve the subnet ID based on tags
-# data "aws_subnets" "private_subnets" {
-#   tags = {
-#     Name = "*PrivateSubnet*"
-#   }
-# }
-
-# data "aws_vpc" "vpc" {
-#   tags = {
-#     Name = "*-VPC"
-#   }
-# }
-
-
-
-
 data "aws_codestarconnections_connection" "connection" {
   name = "github-hemanthreddyjonnala"
 }
-
-
-# data "aws_ssm_parameter" "dbpassword" {
-#   name = "/database/password"
-# #   with_decryption = true
-# }
-
-# data "aws_db_instance" "example_db" {
-#   db_instance_identifier = "${var.db_name}-db"
-# }
-
-# data "aws_iam_policy" "secret_manager_policy" {
-#   name = "secret_manager_policy"
-# }
